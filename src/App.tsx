ce53c6a3fc0bec622cbec0fe5a89479c4c7ffad1
livehass <silva.felipe12@hotmail.com>
2024-02-19 20:44:39 -0300

    added new path for new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/home/Home';
+
 import Navbar from './components/NavBar/NavBar';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
     </>
-);
+  );
 }
 export default App;
