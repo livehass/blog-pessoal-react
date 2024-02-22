289f1c1ffa5868328fa3431c9e61bcc7149cf117
livehass <silva.felipe12@hotmail.com>
2024-02-22 15:25:16 -0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar/NavBar';
 import Footer from './components/footer/Footer';
@@ -9,7 +10,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
-
+import ListaPostagens from './components/postagens/listaPostagem/ListaPostagens';
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
             </Routes>
           </div>
           <Footer />
