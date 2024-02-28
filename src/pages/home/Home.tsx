ef688a19a74999b870289952def4348a99c21795
livehass <silva.felipe12@hotmail.com>
2024-02-28 16:52:35 -0300

    deploy do projeto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -24,7 +24,7 @@ function Home() {
             </div>
   
             <div className="flex justify-center ">
-              <img src="/src/assets/Programming.png" alt="" className='w-2/3' />
+              <img src="../../assets/Programming.png" alt="" className='w-2/3' />
       
             </div>
           </div>
